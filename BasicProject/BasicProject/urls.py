292ffac7d024ecd903de1565868a9b7f2c9c0262
Mikael.Winbladh <mikael.winbladh@logica.com>
2019-10-15 21:32:42 +0200

    Working with the view.

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -5,21 +5,7 @@ Definition of urls for BasicProject.
 from django.conf.urls import include, url
 import HelloDjangoApp.views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
-    # Examples:
-    # url(r'^$', BasicProject.views.home, name='home'),
-    # url(r'^BasicProject/', include('BasicProject.BasicProject.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    
